5741ba4cb950015548c6759042dc8f1cf1cafd39
khenthmatthewblairhamili <khenthmatthewblairhamili.github.io.>
2025-05-02 14:27:47 +0800

    feat: removed search bar

--- src/components/homeviewHS/Header.vue
+++ src/components/homeviewHS/Header.vue
@@ -51,46 +51,7 @@ onMounted(() => {
     
     <!-- Right side icons -->
     <ul class="ml-auto flex items-center">
-      <!-- Search Dropdown -->
-      <li class="mr-1 dropdown">
-        <button 
-          type="button" 
-          class="dropdown-toggle text-gray-400 w-8 h-8 rounded flex items-center justify-center hover:bg-gray-50 hover:text-gray-600"
-          @click.stop="toggleDropdown('search')"
-        >
-          <i class="ri-search-line"></i>
-        </button>
-        <div 
-          class="dropdown-menu shadow-md shadow-black/5 z-30 hidden md:absolute right-0 mt-2 max-w-xs w-full bg-white rounded-md border border-gray-100"
-          :class="{ '!block': activeDropdown === 'search' }"
-        >
-          <form class="p-4 border-b border-b-gray-100">
-            <div class="relative w-full">
-              <input 
-                type="text" 
-                class="py-2 pr-4 pl-10 bg-gray-50 w-full outline-none border border-gray-100 rounded-md text-sm focus:border-blue-500" 
-                placeholder="Enter name to Search Violation"
-              >
-              <i class="ri-search-line absolute top-1/2 left-4 -translate-y-1/2 text-gray-400"></i>
-            </div>
-          </form>
-          <div class="mt-3 mb-2">
-            <div class="text-[13px] font-medium text-gray-400 ml-4 mb-2">Recent Violation</div>
-            <ul class="max-h-64 overflow-y-auto">
-              <li v-for="(item, index) in recentSearches" :key="index">
-                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                  <img :src="item.image" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                  <div class="ml-2">
-                    <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">{{ item.title }}</div>
-                    <div class="text-[11px] text-gray-400">{{ item.price }}</div>
-                  </div>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </li>
-
+      
       <!-- Auth Dropdown -->
         <li class="dropdown ml-3">
           <button 
